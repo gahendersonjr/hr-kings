6f300314180ef19619b718d145d56233916c50f6
Alan Henderson <gahendersonjr@gmail.com>
2019-11-30 20:43:34 -0700

    ann working and very accurate

---
+++ get_normalization_values.py
@@ -0,0 +1,28 @@
+import os
+from enum import Enum
+import csv
+
+class RawInputs(Enum):
+    HC_X = 37
+    HC_Y = 38
+    HIT_DISTANCE = 52
+    LAUNCH_SPEED = 53
+    LAUNCH_ANGLE = 54
+    ESTIMATED_BA = 69
+    ESTIMATED_WOBA = 70
+    LAUNCH_SPEED_ANGLE = 75
+
+dict = {}
+
+for i in RawInputs:
+    dict[i] = 0.0
+
+for filename in os.listdir("C:/Users/Alan/Desktop/finalproject/hr-kings/data/batting_stats"):
+    with open("data/batting_stats/" + filename) as file:
+        reader = csv.reader(file, delimiter=",")
+        next(reader)
+        for row in reader:
+            for i in RawInputs:
+                if row[i.value] != "null" and float(row[i.value]) > dict[i]:
+                    dict[i] = float(row[i.value])
+print dict
--- process_data.py
+++ process_data.py
@@ -2,6 +2,12 @@ import csv
 import numpy as np
 from enum import Enum
 
+class Info(Enum):
+    DATE = 1
+    PLAYER_NAME = 5
+    RESULT = 8
+    PLAY_DESCRIPTION = 15
+
 class RawInputs(Enum):
     HC_X = 37
     HC_Y = 38
@@ -12,11 +18,15 @@ class RawInputs(Enum):
     ESTIMATED_WOBA = 70
     LAUNCH_SPEED_ANGLE = 75
 
-class Info(Enum):
-    DATE = 1
-    PLAYER_NAME = 5
-    RESULT = 8
-    PLAY_DESCRIPTION = 15
+class RawInputDenominators(Enum):
+    HC_X = 248.0
+    HC_Y = 207.46
+    HIT_DISTANCE = 505.0
+    LAUNCH_SPEED = 121.7
+    LAUNCH_ANGLE = 72.5
+    ESTIMATED_BA = 1.0
+    ESTIMATED_WOBA = 2.016
+    LAUNCH_SPEED_ANGLE = 6.0
 
 # TODO: after calling process_data, split into two arrays by classification.
 #       then take proportionate number from each for training, testing and valid data.
@@ -24,7 +34,6 @@ class Info(Enum):
 def process_data(path):
     data = []
     classifications = []
-    raw_input_indices = [i.value for i in RawInputs]
     total_cnt = 0
     missing_hc_cnt = 0
     with open(path) as file:
@@ -36,7 +45,7 @@ def process_data(path):
             if row[RawInputs.HC_Y.value] == "null" or row[RawInputs.HC_X.value] == "null":
                 missing_hc_cnt += 1
                 continue
-            data.append([float(row[i]) for i in raw_input_indices])
+            data.append([float(row[i.value])/get_normalization_denominator(i) for i in RawInputs])
             # Not counting inside the park home runs as they are rare and unpredictable from the data I am using.
             if row[Info.RESULT.value] == "home_run" and "inside-the-park" not in row[Info.PLAY_DESCRIPTION.value]:
                 classifications.append(1)
@@ -45,10 +54,15 @@ def process_data(path):
         #TODO: when analyzing data on trained nets, figure out way to approximate hc_x and hc_y
         print str(missing_hc_cnt) + "/" + str(total_cnt) + " contain missing hit coordinate data and were ommitted."
     data = np.array(data)
-    inputs = [np.reshape(x, (len(raw_input_indices), 1)) for x in data]
+    inputs = [np.reshape(x, (len(RawInputs), 1)) for x in data]
     classifications = np.array(classifications)
     classifications = [vectorized_result(y) for y in classifications]
     return zip(inputs, classifications)
+
+def get_normalization_denominator(raw_data_enum_name):
+    name_as_string = str(raw_data_enum_name)
+    enum_name = name_as_string[name_as_string.find('.')+1:]
+    return RawInputDenominators[enum_name].value
 
 def vectorized_result(result):
     results = np.zeros((2, 1))
